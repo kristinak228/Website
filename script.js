f66c85818e0444e54eb2ea86a9dad6e52065c5ff
Kristina Kolibab <tinytina228@yahoo.com>
2018-01-02 16:43:54 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function ChangeTop(num) {
+			if(num == 0){
+				document.getElementById("colorsch").style.backgroundColor = "#491504"; 
+				document.getElementById("colorsch").style.color = "white"; 
+				document.getElementById("textsch").style.color = "#491504"; 
+			} else {
+				document.getElementById("colorsch").style.backgroundColor = "DarkSlateGray";
+				document.getElementById("colorsch").style.color = "white";
+				document.getElementById("textsch").style.color = "DarkSlateGray";
+			}
+		}
